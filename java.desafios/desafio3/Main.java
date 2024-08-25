c865cca2970e011c18f0ab5c28578611b11d8405
jhoclen <jhoclensantos@gmail.com>
2024-08-25 18:25:19 -0300

    desafio3

--- java.desafios/desafio3/Main.java
+++ java.desafios/desafio3/Main.java
@@ -3,7 +3,12 @@ package desafio3;
 public class Main {
     public static void main(String[] args) {
         
-        LivroDigital livroDigital = new LivroDigital("senhor dos aneis", "eu mesmo", 500, "PDF");
+        LivroDigital livroDigital = new LivroDigital(
+            "senhor dos aneis",
+             "eu mesmo", 
+             500, 
+             "PDF"
+            );
         livroDigital.infoLivro();
     }
     
