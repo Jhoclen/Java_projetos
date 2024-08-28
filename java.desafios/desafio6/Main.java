577730aecd29e0956a89e96056721d85e5a3b23e
jhoclen <jhoclensantos@gmail.com>
2024-08-28 20:29:25 -0300

    desafio6

---
+++ java.desafios/desafio6/Main.java
@@ -0,0 +1,16 @@
+package desafio6;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        Util util = new Util();
+        util.max(5, 5);
+        util.max(5.5, 5.3);
+        util.max("30", "30");
+        
+        
+    }
+    
+    
+}
---
+++ java.desafios/desafio6/Util.java
@@ -0,0 +1,19 @@
+package desafio6;
+
+public class Util {
+    
+    
+    public void  max(int num1, int num2){
+       System.out.println(num1 + num2);  
+    }
+
+    public void max(double num1, double num2){
+        System.out.println(num1 + num2);  
+    }
+
+    public void max(String num1, String num2){
+        System.out.println(Integer.parseInt(num1) + Integer.parseInt(num2));
+    }
+
+    
+}
