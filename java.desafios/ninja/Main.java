17e50207ef91908c1eeed63013dd7c17bc2f19ab
jhoclen <jhoclensantos@gmail.com>
2024-10-14 16:53:55 -0300

    .

--- java.desafios/ninja/Main.java
+++ java.desafios/ninja/Main.java
@@ -1,15 +1,8 @@
 package ninja;
 
 public class Main {
-    public static void main(String[] args) {
-        
-        Uchiha sasuke = new Uchiha();
-        sasuke.nome = "sasuke Uchiha";
-        sasuke.sharingan();
-
-        System.out.println(sasuke.nome);
-      
-        
-    }
+ public static void main(String[] args) {
+    
+ }
     
 }
